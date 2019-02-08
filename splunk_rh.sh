7b5b15541aacf3696d8a3a80ea35c6f509053d87
EduardKiyko <32454964+EduardKiyko@users.noreply.github.com>
2019-02-08 15:18:23 +0200

    Update splunk_rh.sh

--- splunk_rh.sh
+++ splunk_rh.sh
@@ -2,10 +2,10 @@
 yum -y install wget;
 adduser splunk;
 wget -O /tmp/splunk-7.2.4-8a94541dcfac-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.4&product=splunk&filename=splunk-7.2.4-8a94541dcfac-Linux-x86_64.tgz&wget=true'
-tar -xzvf /tmp//tmp/splunk-7.2.4-8a94541dcfac-Linux-x86_64.tgz -C /opt;
+tar -xzvf /tmp/splunk-7.2.4-8a94541dcfac-Linux-x86_64.tgz -C /opt;
 /opt/splunk/bin/splunk start --answer-yes --no-prompt --accept-license --seed-passwd changeme;
 /opt/splunk/bin/splunk stop -auth admin:changeme;
 /opt/splunk/bin/splunk enable boot-start -user splunk;
 chown -R splunk:splunk /opt/splunk;
 runuser -l splunk -c '/opt/splunk/bin/splunk start';
-rm -rf /tmp//tmp/splunk-7.2.4-8a94541dcfac-Linux-x86_64.tgz;
+rm -rf /tmp/splunk-7.2.4-8a94541dcfac-Linux-x86_64.tgz;
